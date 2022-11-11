c0e867fd3c14bba78f74de263c939c0a0e34310a
Maria Dikareva <dikareva.m@gmail.com>
2022-11-11 15:56:51 +0000

    Challenge from TDD

---
+++ lib/challenge.rb
@@ -0,0 +1,4 @@
+def count_words(string)
+    num = string.split(" ").size
+return num
+end
---
+++ spec/challenge_spec.rb
@@ -0,0 +1,7 @@
+require "challenge"
+describe "challenge" do
+    it "returns a number of words in a string" do
+    result = count_words("rose lily violet")
+    expect(result).to eq 3
+end
+end
